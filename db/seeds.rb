d97c84e9d5042d4003c9ac5ae01f6e155d745735
Carlos <awocarlos55@gmail.com>
2023-04-15 10:39:23 +0200

    seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Branch.create(name: "Reseaux et Telecommunications", sigle: "RT", amount:200, duration:5)
+Branch.create(name: "Gestion des Resources Humaines", sigle: "GRH", amount:200, duration:5)
+Branch.create(name: "Banque et Finances", sigle: "BF", amount:200, duration:5)
+Branch.create(name: "Genie Civil", sigle: "GC", amount:200, duration:5)
+Branch.create(name: "Genie Logiciel", sigle: "GL", amount:200, duration:5)
