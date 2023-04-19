9769db26729fea048bd0de8eeb524f839d7c2239
Carlos <awocarlos55@gmail.com>
2023-04-19 13:47:03 +0200

    calculate age with more precision

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,7 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
 
-  def age(born_year)
-    Date.today.year - born_year
+  def age(birth_date)
+    year = Date.today.year - birth_date.year
+    if ( (Date.today.month < birth_date.month ) || (Date.today.month  == birth_date.month && Date.today.day < birth_date.day  ))
+      year = year - 1
+    end
+    year
   end
 end
