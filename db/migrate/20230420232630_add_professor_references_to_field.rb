c1e36d25bcea1e6dbd7014cf4da79623aacedd25
Carlos <awocarlos55@gmail.com>
2023-04-21 22:38:09 +0200

    Add nullabel to professor references in fields

--- db/migrate/20230420232630_add_professor_references_to_field.rb
+++ db/migrate/20230420232630_add_professor_references_to_field.rb
@@ -1,5 +1,5 @@
 class AddProfessorReferencesToField < ActiveRecord::Migration[7.0]
   def change
-    add_reference :fields, :professor, null: false, foreign_key: true
+    add_reference :fields, :professor, null: true, foreign_key: true
   end
 end
